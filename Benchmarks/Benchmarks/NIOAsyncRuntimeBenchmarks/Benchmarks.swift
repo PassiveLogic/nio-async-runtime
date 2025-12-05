0c3905684df61fc0564ec2b0b00093f132617fc3
Scott Marchant <scottm@passivelogic.com>
2025-12-04 17:19:22 -0700

    chore: Run swift formatting.

--- Benchmarks/Benchmarks/NIOAsyncRuntimeBenchmarks/Benchmarks.swift
+++ Benchmarks/Benchmarks/NIOAsyncRuntimeBenchmarks/Benchmarks.swift
@@ -21,99 +21,99 @@ import NIOCore
 private let eventLoop = MultiThreadedEventLoopGroup.singleton.next()
 
 let benchmarks = {
-    let defaultMetrics: [BenchmarkMetric] = [
-        .mallocCountTotal,
-        .contextSwitches,
-        .wallClock,
-    ]
+  let defaultMetrics: [BenchmarkMetric] = [
+    .mallocCountTotal,
+    .contextSwitches,
+    .wallClock,
+  ]
 
-    Benchmark(
-        "MTELG.immediateTasksThroughput",
-        configuration: Benchmark.Configuration(
-            metrics: defaultMetrics,
-            scalingFactor: .mega,
-            maxDuration: .seconds(10_000_000),
-            maxIterations: 5
-        )
-    ) { benchmark in
-        func noOp() {}
-        for _ in benchmark.scaledIterations {
-            eventLoop.execute { noOp() }
+  Benchmark(
+    "MTELG.immediateTasksThroughput",
+    configuration: Benchmark.Configuration(
+      metrics: defaultMetrics,
+      scalingFactor: .mega,
+      maxDuration: .seconds(10_000_000),
+      maxIterations: 5
+    )
+  ) { benchmark in
+    func noOp() {}
+    for _ in benchmark.scaledIterations {
+      eventLoop.execute { noOp() }
+    }
+  }
+
+  Benchmark(
+    "MTELG.scheduleTask(in:_:)",
+    configuration: Benchmark.Configuration(
+      metrics: defaultMetrics,
+      scalingFactor: .mega,
+      maxDuration: .seconds(10_000_000),
+      maxIterations: 5
+    )
+  ) { benchmark in
+    for _ in benchmark.scaledIterations {
+      eventLoop.scheduleTask(in: .hours(1), {})
+    }
+  }
+
+  Benchmark(
+    "MTELG.scheduleCallback(in:_:)",
+    configuration: Benchmark.Configuration(
+      metrics: defaultMetrics,
+      scalingFactor: .mega,
+      maxDuration: .seconds(10_000_000),
+      maxIterations: 5
+    )
+  ) { benchmark in
+    final class Timer: NIOScheduledCallbackHandler {
+      func handleScheduledCallback(eventLoop: some EventLoop) {}
+    }
+    let timer = Timer()
+
+    benchmark.startMeasurement()
+    for _ in benchmark.scaledIterations {
+      let handle = try! eventLoop.scheduleCallback(in: .hours(1), handler: timer)
+    }
+  }
+
+  Benchmark(
+    "Jump to EL and back using execute and unsafecontinuation",
+    configuration: .init(
+      metrics: defaultMetrics,
+      scalingFactor: .kilo
+    )
+  ) { benchmark in
+    for _ in benchmark.scaledIterations {
+      await withUnsafeContinuation { (continuation: UnsafeContinuation<Void, Never>) in
+        eventLoop.execute {
+          continuation.resume()
         }
+      }
+    }
+  }
+
+  final actor Foo {
+    nonisolated public let unownedExecutor: UnownedSerialExecutor
+
+    init(eventLoop: any EventLoop) {
+      self.unownedExecutor = eventLoop.executor.asUnownedSerialExecutor()
     }
 
-    Benchmark(
-        "MTELG.scheduleTask(in:_:)",
-        configuration: Benchmark.Configuration(
-            metrics: defaultMetrics,
-            scalingFactor: .mega,
-            maxDuration: .seconds(10_000_000),
-            maxIterations: 5
-        )
-    ) { benchmark in
-        for _ in benchmark.scaledIterations {
-            eventLoop.scheduleTask(in: .hours(1), {})
-        }
+    func foo() {
+      blackHole(Void())
     }
+  }
 
-    Benchmark(
-        "MTELG.scheduleCallback(in:_:)",
-        configuration: Benchmark.Configuration(
-            metrics: defaultMetrics,
-            scalingFactor: .mega,
-            maxDuration: .seconds(10_000_000),
-            maxIterations: 5
-        )
-    ) { benchmark in
-        final class Timer: NIOScheduledCallbackHandler {
-            func handleScheduledCallback(eventLoop: some EventLoop) {}
-        }
-        let timer = Timer()
-
-        benchmark.startMeasurement()
-        for _ in benchmark.scaledIterations {
-            let handle = try! eventLoop.scheduleCallback(in: .hours(1), handler: timer)
-        }
+  Benchmark(
+    "Jump to EL and back using actor with EL executor",
+    configuration: .init(
+      metrics: defaultMetrics,
+      scalingFactor: .kilo
+    )
+  ) { benchmark in
+    let actor = Foo(eventLoop: eventLoop)
+    for _ in benchmark.scaledIterations {
+      await actor.foo()
     }
-
-    Benchmark(
-        "Jump to EL and back using execute and unsafecontinuation",
-        configuration: .init(
-            metrics: defaultMetrics,
-            scalingFactor: .kilo
-        )
-    ) { benchmark in
-        for _ in benchmark.scaledIterations {
-            await withUnsafeContinuation { (continuation: UnsafeContinuation<Void, Never>) in
-                eventLoop.execute {
-                    continuation.resume()
-                }
-            }
-        }
-    }
-
-    final actor Foo {
-        nonisolated public let unownedExecutor: UnownedSerialExecutor
-
-        init(eventLoop: any EventLoop) {
-            self.unownedExecutor = eventLoop.executor.asUnownedSerialExecutor()
-        }
-
-        func foo() {
-            blackHole(Void())
-        }
-    }
-
-    Benchmark(
-        "Jump to EL and back using actor with EL executor",
-        configuration: .init(
-            metrics: defaultMetrics,
-            scalingFactor: .kilo
-        )
-    ) { benchmark in
-        let actor = Foo(eventLoop: eventLoop)
-        for _ in benchmark.scaledIterations {
-            await actor.foo()
-        }
-    }
+  }
 }
